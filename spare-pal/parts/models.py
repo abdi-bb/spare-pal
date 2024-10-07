d5f2a095332cafcc1ed258393ec51d36b4ee2f26
Abdi Berhe <abdiberhe@gmail.com>
2024-10-07 11:25:19 +0300

    Add models for Customer, Delivery, Logistics, Order, Part, and Supplier

--- spare-pal/parts/models.py
+++ spare-pal/parts/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+from suppliers.models import Supplier
+
+class Part(models.Model):
+    name = models.CharField(max_length=255)
+    part_number = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    stock_quantity = models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- spare-pal/suppliers/models.py
+++ spare-pal/suppliers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Supplier(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
