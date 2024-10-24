4db7a55b67d63d883454e241ebb1557302cdc39a
Abdi Berhe <abdiberhe@gmail.com>
2024-10-24 11:36:03 +0300

    Add choices/ endpoint
    Add choices/ endpoint to the suppliers app.

--- spare-pal/suppliers/api/urls.py
+++ spare-pal/suppliers/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SupplierViewSet, CompanyDetailAddressViewSet, CompanyManagerDetailViewSet
+from .views import SupplierViewSet, CompanyDetailAddressViewSet, CompanyManagerDetailViewSet, get_choices
 
 supplier_router = DefaultRouter()
 supplier_router.register(r'suppliers', SupplierViewSet)
 supplier_router.register(r'company-detail-address', CompanyDetailAddressViewSet)
 supplier_router.register(r'company-manager-detail', CompanyManagerDetailViewSet)
+
+urlpatterns = [
+    path('choices/', get_choices, name='get_choices'),
+]
