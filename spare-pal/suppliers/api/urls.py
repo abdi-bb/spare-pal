491c0c5bbfaaa5854f2fa7f93eece1fa0ec26c59
Abdi Berhe <abdiberhe@gmail.com>
2024-10-24 14:09:22 +0300

    Add a route
    Add a route for the choices endpoint

--- spare-pal/suppliers/api/urls.py
+++ spare-pal/suppliers/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import SupplierViewSet, CompanyDetailAddressViewSet, CompanyManagerDetailViewSet, get_choices
+from .views import SupplierViewSet, CompanyDetailAddressViewSet, CompanyManagerDetailViewSet, GetChoicesAPIView
 
 supplier_router = DefaultRouter()
 supplier_router.register(r'suppliers', SupplierViewSet)
@@ -8,5 +8,5 @@ supplier_router.register(r'company-detail-address', CompanyDetailAddressViewSet)
 supplier_router.register(r'company-manager-detail', CompanyManagerDetailViewSet)
 
 urlpatterns = [
-    path('choices/', get_choices, name='get_choices'),
+    path('choices/', GetChoicesAPIView.as_view(), name='get_choices'),
 ]
