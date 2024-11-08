2bfa0fa3a437fc0b3fecad8da67a8eaf787be552
Abdi Berhe <abdiberhe@gmail.com>
2024-11-08 14:12:11 +0300

    Register object into admin
    Register user object in the admin site.

--- spare-pal/apps/accounts/admin.py
+++ spare-pal/apps/accounts/admin.py
@@ -1,3 +1,46 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile
+from django.utils.translation import gettext_lazy as _
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Define the fields to display in the admin interface
+    list_display = ('email', 'first_name', 'last_name', 'is_supplier', 'is_active', 'date_joined')
+    list_filter = ('is_supplier', 'is_active', 'is_staff')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('-date_joined',)
+    
+    # Define the form for creating and changing users
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_supplier', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_supplier', 'is_active', 'is_staff'),
+        }),
+    )
+    
+    # Ensure that the email field is used as the unique identifier
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    model = Profile
+    list_display = ('user', 'avatar', 'bio', 'activeness', 'created_at')
+    search_fields = ('user__email',)
+    ordering = ('-created_at',)
+
+
+# Register the CustomUser and Profile models in the admin interface
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Profile, ProfileAdmin)
