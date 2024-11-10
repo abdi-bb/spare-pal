f68205796d245918ffdc9b202a46add2b1c52c7d
Abdi Berhe <abdiberhe@gmail.com>
2024-11-10 19:52:13 +0300

    Add filter
    Filter companies using their creators(owners).

--- spare-pal/apps/companies/api/views.py
+++ spare-pal/apps/companies/api/views.py
@@ -5,18 +5,36 @@ from django.shortcuts import get_object_or_404, render
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
+from rest_framework.permissions import IsAuthenticated
 from apps.companies.models import Company, CompanyDetailAddress, CompanyManagerDetail
 from .serializers import CompanySerializer, CompanyDetailAddressSerializer, CompanyManagerDetailSerializer
+from .permissions import IsCompanyOwner
 
 class CompanyViewSet(viewsets.ModelViewSet):
-    queryset = Company.objects.all()
+    queryset = Company.objects.all()  # Default queryset
     serializer_class = CompanySerializer
+    permission_classes = [IsAuthenticated, IsCompanyOwner]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()  # Start with the default queryset
+        # Filter companies created by the logged-in user
+        return queryset.filter(created_by=self.request.user)
+
+    def perform_create(self, serializer):
+        # Ensure that the company is created by the current user
+        serializer.save(created_by=self.request.user)
 
 
 class CompanyDetailAddressViewSet(viewsets.ModelViewSet):
     queryset = CompanyDetailAddress.objects.all()
     serializer_class = CompanyDetailAddressSerializer
+    permission_classes = [IsAuthenticated, IsCompanyOwner]
 
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        # Filter addresses for companies owned by the user
+        return queryset.filter(company__created_by=self.request.user)
+    
     def create(self, request, *args, **kwargs):
         data = request.data.copy()  # Make mutable copy of request data
         company_id = data.pop('company_id', None)
@@ -29,7 +47,7 @@ class CompanyDetailAddressViewSet(viewsets.ModelViewSet):
         except ValueError:
             return Response({"error": "company_id must be a valid integer."}, status=status.HTTP_400_BAD_REQUEST)
 
-        company = get_object_or_404(Supplier, id=company_id)
+        company = get_object_or_404(Company, id=company_id, created_by=request.user)
         
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
@@ -40,6 +58,12 @@ class CompanyDetailAddressViewSet(viewsets.ModelViewSet):
 class CompanyManagerDetailViewSet(viewsets.ModelViewSet):
     queryset = CompanyManagerDetail.objects.all()
     serializer_class = CompanyManagerDetailSerializer
+    permission_classes = [IsAuthenticated, IsCompanyOwner]
+
+    def get_queryset(self):
+        querset = super().get_queryset()
+        # Filter managers for companies owned by the user
+        return querset.filter(company__created_by=self.request.user)
 
     def create(self, request, *args, **kwargs):
         data = request.data.copy()  # Make mutable copy of request data
@@ -53,7 +77,7 @@ class CompanyManagerDetailViewSet(viewsets.ModelViewSet):
         except ValueError:
             return Response({"error": "company_id must be a valid integer."}, status=status.HTTP_400_BAD_REQUEST)
 
-        company = get_object_or_404(Supplier, id=company_id)
+        company = get_object_or_404(Company, id=company_id, created_by=request.user)
 
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
